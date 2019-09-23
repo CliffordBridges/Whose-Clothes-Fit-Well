e30ae6ad1058dea57ce1279851c64438195e3229
CliffordBridges <cliffbridges5@gmail.com>
2019-09-23 09:40:25 -0600

    docstring all functions

--- src/size_review_functions.py
+++ src/size_review_functions.py
@@ -6,8 +6,15 @@ import scipy.stats as stats
 import seaborn as sns
 
 
-#function to convert data entry in weight to integer
 def convert_lbs_to_int(pounds):
+    """
+    Converts weight entry from a string ending in 'lbs' to an int
+    
+    Parameters
+    ----------
+    pounds: str
+        string likely ending in 'lbs' 
+    """    
     if type(pounds) == str:
         if pounds.endswith('lbs'):
             return int(pounds[:-3])
@@ -15,8 +22,15 @@ def convert_lbs_to_int(pounds):
         return
 
     
-#splitting the bust size entry
 def separate_band_and_cup(bust_size):
+    """
+    Splits the int and string (band and cup)
+    
+    Parameters
+    ----------
+    bust_size: str
+        likely a 2 digit integer followed by some nunber of letters
+    """
     if type(bust_size) == str:
         return bust_size[:2]+','+bust_size[2:]
     elif np.isnan(bust_size):
@@ -25,6 +39,14 @@ def separate_band_and_cup(bust_size):
     
 #converting string heights to integer
 def convert_feetinches_to_inches(distance):
+    """
+    Converts height as string with feet and inches to just inches
+    
+    Parameters
+    ----------
+    distance: str
+        likely an integer, single quote, space, integer, double quote
+    """
     if type(distance) == str:
         inches = 0
         for i, ft in enumerate(distance):
@@ -36,15 +58,35 @@ def convert_feetinches_to_inches(distance):
     elif np.isnan(distance):
         return np.NaN
 
-#converting string band entry to integer    
 def convert_to_int(band):
+    """
+    Converts band size to int
+    
+    Parameters
+    ----------
+    band: str
+        band size in inches
+    """
     if type(band)==str:
         return int(band)
     elif band==np.NaN:
         pass
 
-#replace NaN values for either hips or waist to yield moderate risk body proportion
 def hip_waist_ratio(data):
+    """
+    If exactly one measurement for hips or waist is NaN, 
+    replaces NaN value so the resulting hip-waist ratio is 0.85
+
+    Parameters
+    ----------
+    data: list
+        list of ordered pairs (waist, hip)
+    
+    Returns
+    -------
+    lst: list
+        list of "ordered" lists
+    """
     lst = []
     for x in data:
         y = list(x)
@@ -61,12 +103,26 @@ def hip_waist_ratio(data):
     return lst
 
 
-#bootstrap resampling
-np.random.seed(1234)
-def bootstrap( data,m, n,):
+def bootstrap(data, m, n,):
+    """
+    Bootstrap function
+    
+    Parameters
+    ----------
+    data: 1-D array-like or int
+        If an ndarray, a random sample is generated from its elements.
+        If an int, the random sample is generated as if a were np.arange(a)
+        
+    m: int
+        Sample size from data to be selected (with replacement)
+        
+    n: int
+        Number of samples to choose and take mean
+    """
+    np.random.seed(1234)
     result = []
-    for i in range( n):
-        xs = np.random.choice(data, m, replace = True)
-        m1 = np.mean( xs)
-        result.append( m1)
+    for i in range(n):
+    xs = np.random.choice(data, m, replace = True)
+        m1 = np.mean(xs)
+        result.append(m1)
     return result
